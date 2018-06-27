aa4cdca3c91e11cb618ebfbd8312eeebf857b391
Aimeri Baddouh <aimeri@baddouh.me>
2018-06-27 15:06:59 -0400

    added preview of gifs and navigation

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('gifs'))
-// respond with "hello world" when a GET request is made to the homepage
+
 app.get('/', function (req, res) {
     res.render('add.ejs');
-  //res.send('hello world')
 });
 
 app.post('/create', async function (req, res){
@@ -18,17 +17,14 @@ app.post('/create', async function (req, res){
   let name = req.body.name;
   await gif.createGIF(url, name)
   let listOfGifs = fs.readdirSync('./gifs').filter(function(file) { return path.extname(file) === '.gif';})
-  //res.send(`<a href="${name}.gif">${name} GIF</a>`);
  res.render('gif.ejs', { gifs: listOfGifs});
     
 });
 
-app.get('/test', async function (req, res) {
-    //let url = req.params[0];
-    //let name = req.params.name;
-    //await gif.createGIF(url, name);
-    //res.send('gif created', url);
-  })
+app.post('/', function (req, res) {
+    res.render('add.ejs');
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
