75a3b2b562ad8c80a3fb06fb6e1f77bef37124b6
Aimeri Baddouh <aimeri@fashionsloth.slothcrew.com>
2018-06-27 01:45:51 -0400

    added basic skeleton of server functionality -- wip

--- puppeteer-gif-cast.js
+++ puppeteer-gif-cast.js
@@ -1,108 +1,106 @@
-'use strict';
+"use strict";
 //A little setup ahead of time
 const width = 768;
 const height = 600;
-const puppeteer = require('puppeteer');
-const GIFEncoder = require('gif-encoder');
+const puppeteer = require("puppeteer");
+const GIFEncoder = require("gif-encoder");
 const encoder = new GIFEncoder(width, height);
-const fs = require('fs');
-const getPixels = require('get-pixels');
-const workDir = './temp/';
-const gifDir = './gifs/';
+const fs = require("fs");
+const getPixels = require("get-pixels");
+const workDir = "./temp/";
+const gifDir = "./gifs/";
 
 if (!fs.existsSync(workDir)) {
-    fs.mkdirSync(workDir);
-};
+  fs.mkdirSync(workDir);
+}
 
 if (!fs.existsSync(gifDir)) {
-    fs.mkdirSync(gifDir);
-};
+  fs.mkdirSync(gifDir);
+}
 
 let url = process.argv[2];
 let finalGif = process.argv[3];
-let scrollLength = (process.argv[4] != undefined) ? process.argv[4] : 100;
+let scrollLength = process.argv[4] != undefined ? process.argv[4] : 100;
 
+exports.createGIF = async function(passedURL, passedFinal) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
-// Setup gif encoder parameters
-encoder.setFrameRate(60);
-encoder.pipe(file);
-encoder.setQuality(40);
-encoder.setDelay(500);
-encoder.writeHeader();
-encoder.setRepeat(0);
+  await page.setViewport({ width: width, height: height });
+  await page.goto(passedURL);
 
-//Function Declaration
+  async function scrollPage() {
+    await page.evaluate(async scrollLength => {
+      window.scrollBy(0, scrollLength);
+    }, scrollLength);
+  }
 
-function addToGif(images, counter = 0) {
-    let file = require('fs').createWriteStream(gifDir + finalGif + '.gif');
-    getPixels(images[counter], function (err, pixels) {
+  for (let i = 0; i < 60; i++) {
+    await page.screenshot({ path: workDir + i + ".png" });
+    await scrollPage();
+  }
 
-        encoder.addFrame(pixels.data);
-        encoder.read();
-        if (counter === images.length - 1) {
-            encoder.finish();
-            cleanUp(images, function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    fs.rmdirSync(workDir);
-                    console.log('Gif created!');
-                    process.exit(0);
-                }
-            });
+  await browser.close();
 
-        } else {
-            addToGif(images, ++counter);
-        }
-    })
-}
-
-
-
-function cleanUp(listOfPNGs, callback) {
-    let i = listOfPNGs.length;
-    listOfPNGs.forEach(function (filepath) {
-        fs.unlink(filepath, function (err) {
-            i--;
-            if (err) {
-                callback(err);
-                return;
-            } else if (i <= 0) {
-                callback(null);
-            }
-        });
-    });
-}
-
-exports.createGIF = async function (passedURL, passedFinal) {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-
-
-
-    await page.setViewport({ width: width, height: height });
-    await page.goto(passedURL);
-
-    async function scrollPage() {
-        await page.evaluate(async (scrollLength) => {
-            window.scrollBy(0, scrollLength);
-        }, scrollLength);
-    }
-
-    for (let i = 0; i < 60; i++) {
-        await page.screenshot({ path: workDir + i + ".png" });
-        await scrollPage();
-    }
-
-    await browser.close();
-
-    /*Creates array of pngs by listing files inside export folder then
+  /*Creates array of pngs by listing files inside export folder then
     removing extention, sort numerical strings in ascending order, and
     finally adding path and extention to the file. */
-    let listOfPNGs = fs.readdirSync(workDir)
-        .map(a => a.substr(0, a.length - 4) + '')
-        .sort(function (a, b) { return a - b })
-        .map(a => workDir + a.substr(0, a.length) + '.png');
+  let listOfPNGs = fs
+    .readdirSync(workDir)
+    .map(a => a.substr(0, a.length - 4) + "")
+    .sort(function(a, b) {
+      return a - b;
+    })
+    .map(a => workDir + a.substr(0, a.length) + ".png");
 
-    addToGif(listOfPNGs);
+  let file = require("fs").createWriteStream(gifDir + passedFinal + ".gif");
+
+  // Setup gif encoder parameters
+  encoder.setFrameRate(60);
+  encoder.pipe(file);
+  encoder.setQuality(40);
+  encoder.setDelay(500);
+  encoder.writeHeader();
+  encoder.setRepeat(0);
+
+  //Function Declaration
+
+  function addToGif(images, counter = 0) {
+    getPixels(images[counter], function(err, pixels) {
+      encoder.addFrame(pixels.data);
+      encoder.read();
+      if (counter === images.length - 1) {
+        encoder.finish();
+        cleanUp(images, function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            fs.rmdirSync(workDir);
+            console.log("Gif created!");
+            //process.exit(0);
+          }
+        });
+      } else {
+        addToGif(images, ++counter);
+      }
+    });
+  }
+
+  function cleanUp(listOfPNGs, callback) {
+    let i = listOfPNGs.length;
+    listOfPNGs.forEach(function(filepath) {
+      fs.unlink(filepath, function(err) {
+        i--;
+        if (err) {
+          callback(err);
+          return;
+        } else if (i <= 0) {
+          callback(null);
+        }
+      });
+    });
+  }
+
+  addToGif(listOfPNGs);
+  return passedFinal + ".gif";
 };
--- server.js
+++ server.js
@@ -1,2 +1,35 @@
-let createGIF = require('./puppeteer-gif-cast.js')
-create
+let express = require('express');
+let app = express();
+let gif = require('./puppeteer-gif-cast.js');
+const fs = require("fs");
+var path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static('gifs'))
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.render('add.ejs');
+  //res.send('hello world')
+});
+
+app.post('/create', async function (req, res){
+  let url = req.body.url;
+  let name = req.body.name;
+  await gif.createGIF(url, name)
+  let listOfGifs = fs.readdirSync('./gifs').filter(function(file) { return path.extname(file) === '.gif';})
+  //res.send(`<a href="${name}.gif">${name} GIF</a>`);
+ res.render('gif.ejs', { gifs: listOfGifs});
+    
+});
+
+app.get('/test', async function (req, res) {
+    //let url = req.params[0];
+    //let name = req.params.name;
+    //await gif.createGIF(url, name);
+    //res.send('gif created', url);
+  })
+
+app.listen(4000, () => console.log('Example app listening on port 3000!'))
+
+
