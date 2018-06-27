ebd4bbca0227c76dbe6511152529c39678723c6f
Aimeri Baddouh <aimeri@baddouh.me>
2018-06-27 14:50:52 -0400

    Fixing port binding

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.get('/test', async function (req, res) {
     //res.send('gif created', url);
   })
 
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 
