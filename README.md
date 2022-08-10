9eb32ea0051607d65c6e932939ee1e9327781452
Aimeri Baddouh <aimeri@baddouh.me>
2022-08-10 10:46:36 -0400

    implement params and code resiliency by using yargs we can get some argument validation to increase the code resiliency. Implement sane defaults implement width, height, and duration params

--- README.md
+++ README.md
@@ -2,10 +2,16 @@
 
 This little application was written out of necessity for writing documentation about UX/UI. All it does is starts a headless instance of puppeteer, loads the url passed to it, and scrolls the page taking screenshots. At the end it will stitch all the screenshots into one gif as if it was a screencast.
 
-This utility expects two mandatory arguments and one optional:
+This utility expects two mandatory arguments:
 * **URL:** the url of the website you want to screencast
 * **NAME:** the name of the gif (.gif will be automatically appended at the end)
-* ScrollBy: the ammount of scroll to use during the scrolling. If nothing is passed it will scroll by 100 pixels.
+
+Other arguments that can also be passed to control the gif generation:
+* Width: defaults to 768
+* Height: defaults to 600
+* Duration: how long to capture the gif for. Defaults to 60s
+* Scroll: How far down to scroll the page per frame, in pixels. Defaults to 100. Higher numbers means the page will scroll farther per frame, which can be useful for long pages or pages that make heavy use of the browser scroll position.
+
 
 GIFs are saved in the ./gifs/ folder at the end of the process.
 
@@ -22,21 +28,54 @@ initialize the project
 
 to grab a screencast of a website run the command like this:
 
-`npm run cast http://everylastdrop.co.uk every-last-drop 500`
+`npm start http://everylastdrop.co.uk every-last-drop -- --scroll 500`
 
 the generated gif will be located at ./gifs/every-last-drop.gif
 
 ![alt text](https://raw.githubusercontent.com/aimerib/aimerib.github.io/master/images/every-last-drop.gif "UK's Every Last Drop website screencast")
 
 
-the last parameter can be ommited safely like so:
+the scroll parameter can be ommited safely like so:
 
-`npm run cast https://en.wikipedia.org/wiki/JavaScript javascript`
+`npm start http://everylastdrop.co.uk every-last-drop`
 
 ![alt text](https://raw.githubusercontent.com/aimerib/aimerib.github.io/master/images/javascript.gif "Wikipedia's Javascript article")
 
-## TO DO
-* Implement W x H parameters. Right now it defaults to 768x600
-* Implement duration time. Right now it takes 60 screenshots and stops
-* Make code more resilient. Any invalid arguments or parameters break the code as it is.
-* Test it on MacOs and *nix
+***Important***
+
+To ensure that optional parameters are sent correctly to the node process you MUST pass them after `--`
+Please see examples below
+
+## Help
+```
+npm start <url> <name>
+
+starts capturing the gif at <url> and save it with <name>.gif
+
+Positionals:
+  url   the url to caputre a scrolling gif from                         [string]
+  name  the name of the final gif file                                  [string]
+
+Options:
+      --help      Show help                                            [boolean]
+      --version   Show version number                                  [boolean]
+  -w, --width     Sets a width for the captured gif      [number] [default: 768]
+  -h, --height    Sets a height for the captured gif     [number] [default: 600]
+  -s, --scroll    How far to scroll down the per frame   [number] [default: 100]
+  -d, --duration  Sets gif duration in seconds           [number]  [default: 60]
+```
+### Examples:
+```bash
+# capture gif using default values and saves it as every_last_drop.gif
+npm start http://everylastdrop.co.uk every_last_drop
+```
+
+```bash
+# capture gif with custom dimensions
+npm start http://everylastdrop.co.uk every_last_drop -- --width 1080 --height 720
+```
+
+```bash
+# capture gif for 120 seconds
+npm start http://everylastdrop.co.uk every_last_drop -- --duration 120
+```
--- puppeteer-gif-cast.js
+++ puppeteer-gif-cast.js
@@ -1,10 +1,55 @@
 'use strict';
+
 //A little setup ahead of time
-const width = 768;
-const height = 600;
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+
+let args = yargs(hideBin(process.argv))
+    .usage('$0 <url> <name>', 'starts capturing the gif at <url> and save it with <name>.gif', (yargs) => {
+        yargs.positional('url', {
+            describe: 'the url to caputre a scrolling gif from',
+            type: 'string'
+        })
+            .positional('name', {
+                describe: "the name of the final gif file",
+                type: "string"
+            })
+    })
+    .option('width', {
+        alias: 'w',
+        type: 'number',
+        description: 'Sets a width for the captured gif',
+        default: 768
+    })
+    .option('height', {
+        alias: 'h',
+        type: 'number',
+        description: 'Sets a height for the captured gif',
+        default: 600
+    })
+    .option('scroll', {
+        alias: 's',
+        type: 'number',
+        description: 'How far to scroll down the per frame',
+        default: 100
+    })
+    .option('duration', {
+        alias: 'd',
+        type: 'number',
+        description: 'Sets gif duration in seconds',
+        default: 60
+    })
+    .example([
+        ['npm start http://example.com example', 'capture gif using default values and saves it as example.gif'],
+        ['npm start http://example.com example -- --width 1080 --height 720', 'capture gif with custom dimensions'],
+        ['npm start http://example.com example -- --duration 120', 'capture gif for 120 seconds']
+    ])
+    .parse()
+
+const { width, height, name, duration, url, scroll } = args;
+
 const puppeteer = require('puppeteer');
 const GIFEncoder = require('gif-encoder');
-const encoder = new GIFEncoder(width, height);
 const fs = require('fs');
 const getPixels = require('get-pixels');
 const workDir = './temp/';
@@ -18,12 +63,12 @@ if (!fs.existsSync(gifDir)) {
     fs.mkdirSync(gifDir);
 };
 
-let url = process.argv[2];
-let finalGif = process.argv[3];
-let scrollLength = (process.argv[4] != undefined) ? process.argv[4] : 100;
-let file = require('fs').createWriteStream(gifDir + finalGif + '.gif');
+let file = require('fs').createWriteStream(gifDir + name + '.gif');
 
-// Setup gif encoder parameters
+
+
+// Setup gif encoder and parameters
+const encoder = new GIFEncoder(width, height);
 encoder.setFrameRate(60);
 encoder.pipe(file);
 encoder.setQuality(40);
@@ -31,8 +76,7 @@ encoder.setDelay(500);
 encoder.writeHeader();
 encoder.setRepeat(0);
 
-//Function Declaration
-
+// Function Declarations
 function addToGif(images, counter = 0) {
     getPixels(images[counter], function (err, pixels) {
 
@@ -73,7 +117,15 @@ function cleanUp(listOfPNGs, callback) {
     });
 }
 
+// This is where the magic happens:
 (async () => {
+    console.info(`Capturing gif "${name}.gif" with following parameters:`)
+    console.info(`Origin URL: ${url}`)
+    console.info(`Width: ${width}`)
+    console.info(`Height: ${height}`)
+    console.info(`Duration: ${duration}`)
+    console.info(`Scroll length: ${scroll}\n`)
+
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
@@ -85,17 +137,17 @@ function cleanUp(listOfPNGs, callback) {
     async function scrollPage() {
         await page.evaluate(async (scrollLength) => {
             window.scrollBy(0, scrollLength);
-        }, scrollLength);
+        }, scroll);
     }
 
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < duration; i++) {
         await page.screenshot({ path: workDir + i + ".png" });
         await scrollPage();
     }
 
     await browser.close();
 
-    /*Creates array of pngs by listing files inside export folder then
+    /* Creates array of pngs by listing files inside export folder then
     removing extention, sort numerical strings in ascending order, and
     finally adding path and extention to the file. */
     let listOfPNGs = fs.readdirSync(workDir)
